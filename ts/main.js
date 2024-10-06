34ad4d5031dab7cba2f9e22607a145d61bde51c1
Lisa <lizamotorna2004@gmail.com>
2024-10-06 18:56:16 +0300

    added intro

--- ts/main.js
+++ ts/main.js
@@ -1,6 +1,21 @@
-var buttonMenu = document.getElementsByClassName("header__menu")[0];
-var headerBlock = document.getElementsByClassName("header")[0];
+"use strict";
+const buttonMenu = document.getElementsByClassName("header__menu")[0];
+const headerBlock = document.getElementsByClassName("header")[0];
 buttonMenu.onclick = function () {
     buttonMenu.classList.toggle("header__menu-active");
     headerBlock.classList.toggle("header-active");
 };
+const introSlider = $(".intro__slider");
+introSlider.slick({
+    arrows: false,
+    dots: true,
+    draggable: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: false,
+    adaptiveHeight: true,
+    appendDots: $(".intro__pagination"),
+    autoplay: true,
+    autoplaySpeed: 4000
+});
+const introPagItem = document.querySelectorAll(".intro__pagination button");
--- ts/main.ts
+++ ts/main.ts
@@ -5,5 +5,20 @@ const headerBlock = document.getElementsByClassName("header")[0] as HTMLElement;
 
 buttonMenu.onclick = function (): void {
   buttonMenu.classList.toggle("header__menu-active");
-  headerBlock.classList.toggle("header-active")
+  headerBlock.classList.toggle("header-active");
 };
+
+const introSlider = $(".intro__slider") as JQuery<HTMLDivElement>;
+
+introSlider.slick({
+  arrows: false,
+  dots: true,
+  draggable: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  centerMode: false,
+  adaptiveHeight: true,
+  appendDots: $(".intro__pagination"),
+  autoplay: true,
+  autoplaySpeed: 4000
+});
